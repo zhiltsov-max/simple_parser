991c715eef345976fe1dcb6d0131f7d0c3c0d933
zhiltsov-max <zhiltsov.max35@gmail.com>
2018-07-06 20:08:44 +0300

    Initial commit

---
+++ include/parser.hxx
@@ -0,0 +1,130 @@
+#include <iostream>
+#include <exception>
+#include <map>
+#include <string>
+
+
+namespace parsing {
+
+//
+// Implements grammar matching:
+//
+// escape = '\'
+// escaped = escape ( 'r' | 'n' | '\' | 'x' [0-9]{4} )
+// unescaped = [^\\x00-\x20]
+// char = escaped | unescaped
+// value = '"' char* '"'
+//
+// key = [a-zA-Z0-9_]+
+// entry = entry_key ':' ( value | section )
+// entries = entry ( ',' entry )?
+// section = '{' ( entries )? '}'
+// S = (UTF-8 BOM)? section
+//
+// Sample:
+//
+// {
+//   x: "5",
+//   55y : {
+//     _z7: "sdf\n3443\r"
+//   },
+//   qq :"hello"
+// }
+//
+class Lexer {
+public:
+  enum class TokenKind : int {
+    Unknown = 0,
+    Key,
+    Value,
+    SectionBegin,
+    SectionEnd,
+    EntrySeparator,
+    KeyValueSeparator,
+
+    ParseEnd,
+    ParseError,
+
+    _count // keep last
+  };
+
+  class Token {
+  public:
+    using ValueType = std::string;
+
+    Token();
+    Token(TokenKind kind, std::string const& value);
+
+    TokenKind const& getKind() const;
+    ValueType const& getText() const;
+
+    operator bool() const;
+
+    friend bool operator == (std::string const& a, Token const& b);
+    friend bool operator == (Token const& a, std::string const& b);
+
+    friend bool operator == (Token const& a, TokenKind const& b);
+    friend bool operator == (TokenKind const& a, Token const& b);
+
+  private:
+    TokenKind m_kind;
+    ValueType m_value;
+  };
+
+  Lexer(std::istream& is);
+
+  Token const& getCurrent();
+  Token const& getNext();
+
+  bool isFinished() const;
+
+  std::istream::pos_type getPosition() const;
+
+private:
+  class impl;
+
+  char getChar();
+  char peekChar() const;
+
+  std::istream& m_stream;
+  Token m_lastToken;
+};
+
+
+enum class ParsingErrorKind {
+  UnexpectedTokenReceived,
+  UnexpectedDataEnd
+};
+
+struct ParsingError {
+  ParsingErrorKind m_kind;
+  std::istream::pos_type m_position;
+};
+
+class Parser {
+public:
+  using Key = std::string;
+  using Value = std::string;
+  using ParsedTree = std::map<Key, Value>;
+
+  // TODO: use variant or something similar here
+  struct ParsingResult {
+    bool m_success;
+
+    ParsedTree m_tree;
+    ParsingError m_error;
+  };
+
+  Parser(std::istream& is);
+
+  ParsingResult parse();
+
+  static constexpr char s_categorySeparator = ':';
+
+private:
+  class impl;
+
+  Lexer m_lexer;
+};
+
+} // namespace parsing
---
+++ src/parser.cxx
@@ -0,0 +1,717 @@
+#include "parser.hxx"
+
+#include <algorithm>
+#include <exception>
+#include <iostream>
+#include <iterator>
+#include <locale>
+#include <map>
+#include <numeric>
+#include <stack>
+#include <string>
+#include <vector>
+
+
+namespace parsing {
+
+class Exception : public std::exception {
+public:
+  Exception(std::string const& message = "");
+
+  char const* what() const noexcept override;
+protected:
+  std::string m_message;
+};
+
+Exception::Exception(std::string const& message)
+  : m_message(message)
+{}
+
+char const* Exception::what() const noexcept
+{
+  return m_message.c_str();
+}
+
+
+Lexer::Token::Token()
+  : m_kind(TokenKind::Unknown)
+  , m_value()
+{}
+
+Lexer::Token::Token(TokenKind kind, std::string const& value)
+  : m_kind(kind)
+  , m_value(value)
+{}
+
+Lexer::Token::operator bool() const
+{
+  return (m_kind != TokenKind::Unknown);
+}
+
+Lexer::TokenKind const& Lexer::Token::getKind() const
+{
+  return m_kind;
+}
+
+Lexer::Token::ValueType const& Lexer::Token::getText() const
+{
+  return m_value;
+}
+
+bool operator == (std::string const& a, Lexer::Token const& b)
+{
+  return (b.m_kind == Lexer::TokenKind::Value) && (a == b.m_value);
+}
+
+bool operator == (Lexer::Token const& a, std::string const& b)
+{
+  return b == a;
+}
+
+bool operator == (Lexer::Token const& a, Lexer::TokenKind const& b)
+{
+  return a.getKind() == b;
+}
+
+bool operator == (Lexer::TokenKind const& a, Lexer::Token const& b)
+{
+  return b == a;
+}
+
+
+struct Lexer::impl {
+  static Token readToken(Lexer& lexer)
+  {
+    if (lexer.getPosition() == 0) {
+      skipBom(lexer);
+    }
+
+    skipIgnored(lexer);
+
+    if (!lexer.m_stream.good()) {
+      if (lexer.m_stream.eof()) {
+        return { TokenKind::ParseEnd, "" };
+      } else {
+        fail("Input stream error");
+      }
+    }
+
+    if (check(lexer, s_sectionBegin)) {
+      return readSectionBegin(lexer);
+    } else if (checkIsKey(lexer)) {
+      return readKey(lexer);
+    } else if (check(lexer, s_keySeparator)) {
+      return readKeySeparator(lexer);
+    } else if (check(lexer, s_entrySeparator)) {
+      return readEntrySeparator(lexer);
+    } else if (check(lexer, s_sectionEnd)) {
+      return readSectionEnd(lexer);
+    } else if (check(lexer, s_valueBegin)) {
+      return readValue(lexer);
+    } else {
+      return { TokenKind::ParseError, "Syntax error" };
+    }
+  }
+
+  static bool checkIsKey(Lexer& lexer)
+  {
+    return isKeyBeginner(lexer.peekChar());
+  }
+
+  static void skipIgnored(Lexer& lexer)
+  {
+    char symbol = lexer.peekChar();
+    while (isIgnored(symbol)) {
+      symbol = lexer.getChar();
+    }
+  }
+
+  static void skipBom(Lexer& lexer)
+  {
+    constexpr char utf8bom[] = {
+      static_cast<char>(0xEF),
+      static_cast<char>(0xBB),
+      static_cast<char>(0xBF)
+    };
+
+    if (check(lexer, utf8bom[0])) {
+      lexer.getChar();
+      expect(lexer, { utf8bom[1], utf8bom[2] }, "Wrong BOM");
+    }
+  }
+
+  static bool isIgnored(char c)
+  {
+    return std::isspace(c, s_utf8locale)
+        || std::iscntrl(c, s_utf8locale);
+  }
+
+  static bool isKeyBeginner(char c)
+  {
+    return std::isalpha(c, s_defaultLocale)
+        || std::isdigit(c, s_defaultLocale)
+        || (c == '_');
+  }
+
+  static Token readKey(Lexer& lexer)
+  {
+    auto isKeyInternal = [&] (char c) {
+      return std::isalpha(c, s_defaultLocale)
+        || std::isdigit(c, s_defaultLocale)
+        || (c == '_');
+    };
+
+    std::string buffer;
+    char c = lexer.peekChar();
+    while ((c != s_keySeparator) && isKeyInternal(c)) {
+      buffer.append({ c });
+      c = lexer.getChar();
+    }
+    return { TokenKind::Key, std::move(buffer) };
+  }
+
+  static Token readValue(Lexer& lexer)
+  {
+    auto readEscaped = [&] () -> std::string {
+      if (check(lexer, 'n')) {
+        lexer.getChar();
+        return { '\n' };
+      } else if (check(lexer, 'r')) {
+        lexer.getChar();
+        return { '\r' };
+      } else if (check(lexer, s_escape)) {
+        lexer.getChar();
+        return { s_escape };
+      } else if (check(lexer, 'x')) {
+        constexpr size_t maxEscapeSequenceLength = 4;
+        char escapeSequence[maxEscapeSequenceLength] = { 0 };
+        for (char& elem : escapeSequence) {
+          elem = lexer.getChar();
+        }
+        return { std::begin(escapeSequence), std::end(escapeSequence) };
+      }
+
+      fail("Unknown escape sequence");
+    };
+
+    auto readUnescaped = [&] () -> char {
+      char const c = lexer.peekChar();
+      if (std::iscntrl(c, s_utf8locale)) {
+        fail("Unexpected character");
+      }
+      return c;
+    };
+
+    expect(lexer, s_valueBegin, "Expected value");
+
+    std::string buffer;
+    char c = lexer.peekChar();
+    while (c != s_valueEnd) {
+      if (check(lexer, s_escape)) {
+        lexer.getChar();
+        buffer.append(readEscaped());
+      } else {
+        buffer.append({ readUnescaped() });
+      }
+      c = lexer.getChar();
+    }
+
+    return { TokenKind::Value, std::move(buffer) };
+  }
+
+  static Token readSectionBegin(Lexer& lexer)
+  {
+    expect(lexer, s_sectionBegin, "Expected section begin");
+    return { TokenKind::KeyValueSeparator, { s_sectionBegin } };
+  }
+
+  static Token readSectionEnd(Lexer& lexer)
+  {
+    expect(lexer, s_sectionEnd, "Expected section end");
+    return { TokenKind::KeyValueSeparator, { s_sectionEnd } };
+  }
+
+  static Token readKeySeparator(Lexer& lexer)
+  {
+    expect(lexer, s_keySeparator, "Expected key separator");
+    return { TokenKind::KeyValueSeparator, { s_keySeparator } };
+  }
+
+  static Token readEntrySeparator(Lexer& lexer)
+  {
+    expect(lexer, s_entrySeparator, "Expected entry separator");
+    return { TokenKind::EntrySeparator, { s_entrySeparator } };
+  }
+
+
+  static bool expect(Lexer& lexer, std::string const& expected,
+    char const* failMessage)
+  {
+    if (!check(lexer, expected)) {
+      fail(failMessage);
+    }
+  }
+
+  static bool expect(Lexer& lexer, char expected,
+    char const* failMessage)
+  {
+    if (!check(lexer, expected)) {
+      fail(failMessage);
+    }
+  }
+
+  static bool check(Lexer& lexer, std::string const& expected)
+  {
+    auto startPos = lexer.m_stream.tellg();
+
+    auto iExpected = expected.begin();
+    auto const iExpectedEnd = expected.end();
+    char c = lexer.peekChar();
+    while ((iExpected != iExpectedEnd) && (c == *iExpected)) {
+        ++iExpected;
+        c = lexer.getChar();
+    }
+    bool const result = iExpected == iExpectedEnd;
+
+    lexer.m_stream.seekg(startPos);
+
+    return result;
+  }
+
+  static bool check(Lexer& lexer, char expected)
+  {
+    return lexer.peekChar() == expected;
+  }
+
+  static Token fail(std::string const& message)
+  {
+    throw Exception(message);
+  }
+
+
+  static std::locale const s_utf8locale;
+  static std::locale const s_defaultLocale;
+  static constexpr char s_keySeparator = ':';
+  static constexpr char s_entrySeparator = ',';
+  static constexpr char s_sectionBegin = '{';
+  static constexpr char s_sectionEnd = '}';
+  static constexpr char s_valueBegin = '"';
+  static constexpr char s_valueEnd = '"';
+  static constexpr char s_escape = '\\';
+};
+
+std::locale const Lexer::impl::s_utf8locale = std::locale("en_US.UTF-8");
+std::locale const Lexer::impl::s_defaultLocale = std::locale("");
+
+Lexer::Lexer(std::istream& is)
+  : m_stream(is)
+  , m_lastToken()
+{}
+
+Lexer::Token const& Lexer::getCurrent()
+{
+  if (!m_lastToken || !isFinished()) {
+    return getNext();
+  }
+  return m_lastToken;
+}
+
+Lexer::Token const& Lexer::getNext()
+{
+  if (isFinished()) {
+    return m_lastToken;
+  }
+
+  try {
+    m_lastToken = impl::readToken(*this);
+  } catch (Exception const& e) {
+    m_lastToken = Token(TokenKind::ParseError,
+      "Parse error at position " + std::to_string(m_stream.tellg()) + ": " +
+      e.what());
+  }
+  return m_lastToken;
+}
+
+bool Lexer::isFinished() const
+{
+  return m_lastToken.getKind() == TokenKind::ParseEnd;
+}
+
+std::istream::pos_type Lexer::getPosition() const
+{
+  return m_stream.tellg();
+}
+
+char Lexer::getChar()
+{
+  if (m_stream.eof()) {
+    throw Exception("Unexpected end of data");
+  } else if (m_stream.bad()) {
+    throw Exception("Internal stream error");
+  }
+  return m_stream.get();
+}
+
+char Lexer::peekChar() const
+{
+  return m_stream.peek();
+}
+
+
+struct Parser::impl {
+  // Grammar is decomposed to:
+  //
+  // Start = Section
+  // Section = SectionBegin Entries SectionEnd
+  // SectionBegin = '{'
+  // SectionEnd = '}'
+  // Entries = ( Entry NextEntry )?
+  // Entry = Key KeyValueSeparator Value
+  // NextEntry = ( EntrySeparator Entry )?
+  // EntrySeparator = ','
+  // Key = key
+  // KeyValueSeparator = ':'
+  // Value = Section | TextValue
+  // TextValue = value
+
+  // Mapping from decomposed grammar to internal parser states
+  enum class StateKind {
+    Start = 0,
+    Section,
+    SectionBegin,
+    SectionEnd,
+    Entry,
+    NextEntry,
+    EntrySeparator,
+    Entries,
+    Key,
+    Value,
+    KeyValueSeparator,
+    TextValue,
+
+    _count // keep last
+  };
+
+  // Set of possible token parsing products
+  enum class ProductKind {
+    SectionBegin,
+    SectionEnd,
+    Entry,
+    Key,
+    Value
+  };
+
+  // Token parsing product
+  struct Product {
+    ProductKind m_kind;
+    std::string m_value;
+  };
+
+  using TokenKind = Lexer::TokenKind;
+  using Token = Lexer::Token;
+
+  // Transition function result
+  enum class ActionKind {
+    Produce, // accept current token and produce some symbols
+    Expect, // accept current token and expect some new tokens
+    Fail // reject current token and return parse error
+  };
+
+  // TODO: implement using variant
+  struct Action {
+    ActionKind m_kind;
+
+    struct Fail {
+      ParsingErrorKind m_error;
+      StateKind m_state;
+      TokenKind m_receivedToken;
+      std::istream::pos_type m_position;
+    };
+    struct Produce {
+      std::vector<Product> m_producedSymbols;
+    };
+    struct Expect {
+      std::vector<StateKind> m_expectedProductions;
+    };
+
+    Fail m_failure;
+    Produce m_production;
+    Expect m_expectation;
+
+    static Action expect(std::vector<StateKind> states)
+    {
+      Action action;
+      action.m_kind = ActionKind::Expect;
+      action.m_expectation.m_expectedProductions = std::move(states);
+      return action;
+    }
+
+    static Action produce(std::vector<Product> products)
+    {
+      Action action;
+      action.m_kind = ActionKind::Produce;
+      action.m_production.m_producedSymbols = std::move(products);
+      return action;
+    }
+
+    static Action fail(StateKind state, TokenKind receivedToken,
+      std::istream::pos_type position,
+      ParsingErrorKind error = ParsingErrorKind::UnexpectedTokenReceived)
+    {
+      Action action;
+      action.m_kind = ActionKind::Fail;
+      action.m_failure.m_error = error;
+      action.m_failure.m_state = state;
+      action.m_failure.m_position = position;
+      action.m_failure.m_receivedToken = receivedToken;
+      return action;
+    }
+  };
+
+  // Transition function
+  static Action doTransition(StateKind const& state, Lexer& lexer)
+  {
+    auto check = [&] (TokenKind const& kind) {
+      auto const& token = lexer.getCurrent();
+      return token.getKind() == kind;
+    };
+    auto consume = [&] () -> Token const& {
+      auto const& current = lexer.getCurrent();
+      lexer.getNext();
+      return current;
+    };
+
+    if (check(TokenKind::ParseError)) {
+      return Action::fail(state,
+        lexer.getCurrent().getKind(), lexer.getPosition());
+    }
+
+    switch (state) {
+      case StateKind::Start:
+        return Action::expect({ StateKind::Section });
+
+      case StateKind::Section:
+        return Action::expect({
+          StateKind::SectionBegin,
+          StateKind::Entries,
+          StateKind::SectionEnd
+        });
+
+      case StateKind::SectionBegin:
+        if (check(TokenKind::SectionBegin)) {
+          consume();
+          return Action::produce({
+            { ProductKind::SectionBegin, "" }
+          });
+        }
+
+      case StateKind::SectionEnd:
+        if (check(TokenKind::SectionEnd)) {
+          consume();
+          return Action::produce({
+            { ProductKind::SectionEnd, "" }
+          });
+        }
+
+      case StateKind::Entries:
+        if (check(TokenKind::Key)) {
+          return Action::expect({ StateKind::Entry, StateKind::NextEntry });
+        } else {
+          return Action::expect({ /* none */ });
+        }
+
+      case StateKind::Entry:
+        return Action::expect({
+          StateKind::Key,
+          StateKind::KeyValueSeparator,
+          StateKind::Value
+        });
+
+      case StateKind::Key:
+        if (check(TokenKind::Key)) {
+          Token token = consume();
+          return Action::produce({
+            { ProductKind::Entry, "" },
+            { ProductKind::Key, token.getText() }
+          });
+        }
+
+      case StateKind::KeyValueSeparator:
+        if (check(TokenKind::KeyValueSeparator)) {
+          consume();
+          return Action::produce({ /* none */ });
+        }
+
+      case StateKind::Value:
+        if (check(TokenKind::Value)) {
+          return Action::expect({ StateKind::TextValue });
+        } else if (check(TokenKind::SectionBegin)) {
+          return Action::expect({ StateKind::Section });
+        }
+
+      case StateKind::TextValue:
+        if (check(TokenKind::Value)){
+          auto const& token = consume();
+          return Action::produce({
+            { ProductKind::Value, token.getText() }
+          });
+        }
+
+      case StateKind::NextEntry:
+        if (check(TokenKind::EntrySeparator)) {
+          return Action::expect({ StateKind::Entry });
+        } else {
+          return Action::expect({ /* none */ });
+        }
+
+      // no default for warning
+    }
+
+    return Action::fail(state,
+      lexer.getCurrent().getKind(), lexer.getPosition());
+  }
+
+  // Factory function for errors
+  static ParsingError makeParsingError(Action::Fail const& failure)
+  {
+    ParsingError error;
+    error.m_kind = failure.m_error;
+    error.m_position = failure.m_position;
+    return error;
+  }
+
+  // Function to create category name in parsed tree
+  static Key join(std::vector<Key> const& parts, std::string const& glue)
+  {
+    size_t const length = std::accumulate(parts.begin(), parts.end(), 0,
+      [&] (auto const& sum, auto const& part) {
+        return sum + part.size() + glue.size();
+      });
+    Key result;
+    result.reserve(length);
+    std::for_each(parts.begin(), parts.end(), [&] (auto const& part) {
+      result.append(part).append(glue);
+    });
+    return result;
+  }
+
+  // Function to create resulting parsing tree
+  static ParsedTree makeOutputTree(std::vector<Product> const& outputSequence)
+  {
+    // NOTE: assuming sequence satisfies grammar
+
+    ParsedTree tree;
+
+    std::vector<Key> sectionsStack;
+    Key lastKey;
+
+    for (auto const& product : outputSequence) {
+      switch (product.m_kind) {
+        case ProductKind::SectionBegin:
+        {
+          sectionsStack.push_back(lastKey);
+          auto category = join(sectionsStack, { s_categorySeparator });
+          tree.emplace(std::move(category), "");
+          break;
+        }
+
+        case ProductKind::SectionEnd:
+          sectionsStack.pop_back();
+          break;
+
+        case ProductKind::Entry:
+          // none
+          break;
+
+        case ProductKind::Key:
+          lastKey = product.m_value;
+          break;
+
+        case ProductKind::Value:
+        {
+          auto category = join(sectionsStack, { s_categorySeparator });
+          tree.emplace(std::move(category), product.m_value);
+          break;
+        }
+
+        // no default for warning
+      }
+    }
+
+    return tree;
+  }
+};
+
+Parser::Parser(std::istream& is)
+  : m_lexer(is)
+{}
+
+Parser::ParsingResult Parser::parse() {
+  // Parses the grammar as LL(1) using predictive LL(1) parser.
+
+  using Action = impl::Action;
+  using ActionKind = impl::ActionKind;
+  using StateKind = impl::StateKind;
+  using Product = impl::Product;
+
+  ParsingResult result;
+
+  std::stack<StateKind> states;
+  states.push(StateKind::Start);
+
+  std::vector<Product> outputSequence;
+
+  auto fail = [&] (Action::Fail const& failure) {
+    result.m_error = impl::makeParsingError(failure);
+  };
+
+  auto expect = [&] (Action::Expect const& expectation) {
+    std::for_each(
+      expectation.m_expectedProductions.rbegin(),
+      expectation.m_expectedProductions.rend(),
+      [&] (auto const& entry) {
+        states.push(entry);
+      });
+  };
+
+  auto accept = [&] (Action::Produce const& production) {
+    for (auto const& entry : production.m_producedSymbols) {
+      outputSequence.push_back(entry);
+    }
+  };
+
+  auto doAction = [&] (Action const& action) -> bool {
+    switch (action.m_kind) {
+      case ActionKind::Expect:
+        expect(action.m_expectation);
+        return true;
+
+      case ActionKind::Produce:
+        accept(action.m_production);
+        return true;
+
+      case ActionKind::Fail:
+        fail(action.m_failure);
+        return false;
+
+      // no default for warning
+    }
+    return false;
+  };
+
+  result.m_success = true;
+  while (result.m_success && !states.empty()) {
+    auto state = states.top();
+    states.pop();
+    auto action = impl::doTransition(state, m_lexer);
+    result.m_success = doAction(action);
+  }
+
+  if (result.m_success) {
+    result.m_tree = impl::makeOutputTree(outputSequence);
+  }
+
+  return result;
+}
+
+} // namespace parsing
